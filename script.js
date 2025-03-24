4813425bd05ba6068b4c170d08670ebceac9fe4c
ISaad <isma.saad@gmail.com>
2025-03-24 10:36:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,16 @@ fetch("palabras.txt")
 
 document.getElementById("inputTexto").addEventListener("input", function () {
   const prefijo = this.value.toLowerCase();
+
+   // Si la entrada está vacía, no mostrar resultados
+  if (prefijo === "") {
+    document.getElementById("resultado").innerHTML = "";
+    return;  // Detener la ejecución si la entrada está vacía
+  }
+   
   const resultados = palabras.filter(p => p.startsWith(prefijo));
   
   const lista = document.getElementById("resultado");
+  if 
   lista.innerHTML = resultados.map(p => `<li>${p}</li>`).join("");
 });
